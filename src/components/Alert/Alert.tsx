d048ef7679d6fc1823b2596372ed8a58f07259b9
Isabella Pegoraro <isabella.marcondes@madeiramadeira.com.br>
2024-07-18 22:14:51 -0300

    feat: adiciona componente de alert

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,28 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import "./Alert.css";
+
+type TAlert = {
+  open: boolean;
+  setOpenAlert: Dispatch<SetStateAction<boolean>>;
+};
+
+function Alert({ open, setOpenAlert }: TAlert): JSX.Element {
+  const [className, setClassName] = useState<string>("alert");
+
+  useEffect(() => {
+    if (open) {
+      setTimeout(() => {
+        setClassName("alert open");
+      }, 1000);
+    }
+
+    setTimeout(() => {
+      setClassName("alert");
+      setOpenAlert(false);
+    }, 4000);
+  }, [open, setOpenAlert]);
+
+  return <div className={className}>Candidatura Enviada com Sucesso!</div>;
+}
+
+export default Alert;
