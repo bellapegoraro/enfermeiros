d248408e64a3133b780e1b1b0251d33be1100a19
Isabella Pegoraro <isabella.marcondes@madeiramadeira.com.br>
2024-07-18 22:17:31 -0300

    feat: adiciona compoentes ao app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+import Unities from "./components/Unities/Unities";
+import Unity from "./components/Unity";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <Unities />
+            </>
+          }
+        />
+        <Route path="/unidade/:id" element={<Unity />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
